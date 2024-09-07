2c025f2d1dfc6474d4f02dac8c3afa41dfa71f38
himanshuranjandixit <himanshuranjandixit@gmail.com>
2024-09-07 09:04:49 +0530

    Time: 12 ms (34.04%), Space: 10.8 MB (65.68%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,16 +9,26 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if( head==NULL || head->next==NULL) return false;
-        multiset<ListNode*>ms;
-        ms.insert(head);
-        ListNode* temp=head->next;
-        while(temp!=NULL){
-            if(ms.find(temp)!=ms.end()){
-                return true;
-            }
-            ms.insert(temp);
-            temp=temp->next;
+        // if( head==NULL || head->next==NULL) return false;
+        // multiset<ListNode*>ms;
+        // ms.insert(head);
+        // ListNode* temp=head->next;
+        // while(temp!=NULL){
+        //     if(ms.find(temp)!=ms.end()){
+        //         return true;
+        //     }
+        //     ms.insert(temp);
+        //     temp=temp->next;
+        // }
+        // return false;
+        ListNode* slow = head;
+        ListNode* fast= head;
+        while(fast !=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next;
+            if(fast!=NULL) fast=fast->next;
+           
+            if(slow==fast) return true;
         }
         return false;
         
