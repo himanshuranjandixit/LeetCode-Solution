c2c9a9d684ac039d70b001adb4ae9dc3819102e2
himanshuranjandixit <himanshuranjandixit@gmail.com>
2024-09-30 14:57:43 +0530

    Time: 0 ms (100%), Space: 7.1 MB (100%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
-        while(n>1){
-            if(n%2!=0){
-                return false;
-            }
-            n/=2;
-        }
-        return true;
+        // while(n>1){
+        //     if(n%2!=0){
+        //         return false;
+        //     }
+        //     n/=2;
+        // }
+        // return true;
+        int ans=n&(n-1);
+        if(ans == 0) return true;
+        return false;
         
     }
 };
