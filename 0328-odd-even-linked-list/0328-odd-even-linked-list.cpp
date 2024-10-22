b03b6a8ebbe0083c27b965c1f3258a5baf5ca9e2
himanshuranjandixit <himanshuranjandixit@gmail.com>
2024-10-23 04:46:33 +0530

    Time: 0 ms (100%), Space: 15.6 MB (7.69%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,18 +11,42 @@
 class Solution {
 public:       
     ListNode* oddEvenList(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
-       ListNode* even =head;
-       ListNode* odd=head->next;
-       ListNode* jadoo=odd;
-       while(odd!=NULL && odd->next!=NULL){
-        even->next=odd->next;
-        even=even->next;
-        odd->next=even->next;
-        odd=odd->next;
-       }
-       even->next=jadoo;
-       return head;
+    if(head==NULL || head->next==NULL) return head;
+    //    ListNode* even =head;
+    //    ListNode* odd=head->next;
+    //    ListNode* jadoo=odd;
+    //    while(odd!=NULL && odd->next!=NULL){
+    //     even->next=odd->next;
+    //     even=even->next;
+    //     odd->next=even->next;
+    //     odd=odd->next;
+    //    }
+    //    even->next=jadoo;
+    //    return head;
+    vector<int>even;
+    ListNode* temp=head;
+    while(temp!=NULL && temp->next!=NULL){
+        even.push_back(temp->val);
+        temp=temp->next->next;
+    }
+    if(temp) even.push_back(temp->val);
+    ListNode* temp1=head->next;
+    while(temp1!=NULL && temp1->next!=NULL){
+        even.push_back(temp1->val);
+        temp1=temp1->next->next;
+    }
+    if(temp1) even.push_back(temp1->val);
+    ListNode* ans=head;
+    int i=0;
+    while(i<even.size()){
+        ans->val=even[i];
+        ans=ans->next;
+        i++;
+    }
+    return head;
+    
+
+    
     }
     
 };
