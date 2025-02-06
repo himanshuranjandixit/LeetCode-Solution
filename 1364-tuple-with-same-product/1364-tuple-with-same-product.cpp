726fedaffcf0b66ef8abe686cf872d7c1986d3a9
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-02-06 10:24:01 +0530

    Time: 340 ms (33.23%), Space: 84.9 MB (67.71%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
         int ans=0;
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 mpp[nums[i]*nums[j]]++;
