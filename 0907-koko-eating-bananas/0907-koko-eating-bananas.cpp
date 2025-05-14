68358e586963503894d48c0a53fe95ce0f2fb0f6
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-05-14 16:39:25 +0530

    Time: 2288 ms (5.39%), Space: 22.8 MB (73.73%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long ans=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<piles.size();i++){
+            ans+=piles[i];
+            maxi=max(maxi,piles[i]);
+        }
+        int ansi=ans/h;
+        if(ans%h!=0) ansi++;
+        ans=ansi;
+        int mini=0;
+        for(int i=ans;i<=maxi;i++){
+            int val=0;
+            for(int j=0;j<piles.size();j++){
+                val+=piles[j]/i;
+                if(piles[j]%i!=0)val++;
+            }
+            if(val<=h){
+                mini=i;
+                break;
+            }
+        }
+        return mini;
+
+        
+    }
+};
