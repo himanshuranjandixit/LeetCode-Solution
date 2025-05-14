54562aff496064fcbf878794a88be52223ea2c42
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-05-14 16:45:38 +0530

    Time: 11 ms (44.25%), Space: 22.8 MB (73.73%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -10,19 +10,34 @@ public:
         int ansi=ans/h;
         if(ans%h!=0) ansi++;
         ans=ansi;
-        int mini=0;
-        for(int i=ans;i<=maxi;i++){
-            int val=0;
-            for(int j=0;j<piles.size();j++){
-                val+=piles[j]/i;
-                if(piles[j]%i!=0)val++;
+        // int mini=0;
+        // for(int i=ans;i<=maxi;i++){
+        //     int val=0;
+        //     for(int j=0;j<piles.size();j++){
+        //         val+=piles[j]/i;
+        //         if(piles[j]%i!=0)val++;
+        //     }
+        //     if(val<=h){
+        //         mini=i;
+        //         break;
+        //     }
+        // }
+        // return mini;
+        int low = ans;
+        int high = maxi;
+        while(low<high){
+            int mid = (low+high)/2;
+            int val = 0;
+            for(int i=0;i<piles.size();i++){
+                val+=piles[i]/mid;
+                if(piles[i]%mid!=0) val++;
+        
             }
-            if(val<=h){
-                mini=i;
-                break;
-            }
+            if(val<=h) high=mid;
+            else low=mid+1;
+
         }
-        return mini;
+        return low;
 
         
     }
