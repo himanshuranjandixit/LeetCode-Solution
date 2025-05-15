53f1b898b6e9899b1e774ff4fc42f2419e2e1cb5
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-05-15 08:02:21 +0530

    Time: 4 ms (14.55%), Space: 30.1 MB (25.85%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
-        vector<string>v0,v1;
-        bool num0=true;
-        bool num1=true;
-        for(int i=0;i<words.size();i++){
-            if(groups[i]==0 && num0){
-                v0.push_back(words[i]);
-                num0=false;
-            }
-            else if(groups[i]==1 && !num0){
-                v0.push_back(words[i]);
-                num0=true;
-            }
-            if(groups[i]==1 && num1){
-                v1.push_back(words[i]);
-                num1=false;
-            }
-            else if(groups[i]==0 && !num1){
-                v1.push_back(words[i]);
-                num1=true;
-            }
+        // vector<string>v0,v1;
+        // bool num0=true;
+        // bool num1=true;
+        // for(int i=0;i<words.size();i++){
+        //     if(groups[i]==0 && num0){
+        //         v0.push_back(words[i]);
+        //         num0=false;
+        //     }
+        //     else if(groups[i]==1 && !num0){
+        //         v0.push_back(words[i]);
+        //         num0=true;
+        //     }
+        //     if(groups[i]==1 && num1){
+        //         v1.push_back(words[i]);
+        //         num1=false;
+        //     }
+        //     else if(groups[i]==0 && !num1){
+        //         v1.push_back(words[i]);
+        //         num1=true;
+        //     }
 
 
+        // }
+        // if(v0.size()>v1.size()) return v0;
+        // return v1;
+        vector<string>ans;
+        ans.push_back(words[0]);
+        for(int i=1;i<words.size();i++){
+            if(groups[i]!=groups[i-1]) ans.push_back(words[i]);
         }
-        if(v0.size()>v1.size()) return v0;
-        return v1;
+        return ans;
         
     }
 };
