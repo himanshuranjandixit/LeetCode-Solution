8dc59563d973cc04c4e77bb1543a252ba148da60
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-05-16 14:32:21 +0530

    Time: 53 ms (5.05%), Space: 63.8 MB (5.01%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isvalid(string s) {
+        stack<char> st;
+        for(char c : s) {
+            if(c == '(') {
+                st.push(c);
+            } else {
+                if(st.empty()) return false;
+                char top = st.top();
+                if((c == ')' && top != '(')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+    void dp(vector<string>& ans, int n , string s, int left, int right){
+        if(left==n && right==n){
+            if(isvalid(s)) ans.push_back(s);
+            return ;
+        }
+        if(left>n || right>n) return;
+        dp(ans,n,s+'(',left+1,right);
+        dp(ans,n,s+')',left,right+1);
+
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        dp(ans,n,"",0,0);
+        return ans;
+        
+    }
+};
