7cdfe99a81b54cc018b01f9504a201c691232313
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-05-19 18:52:28 +0530

    Time: 0 ms (100%), Space: 13 MB (13.87%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        // vector<int>v;
-        // for(int i=0;i<nums1.size();i++){
-        //     int val=-1;
-        //     for(int j=0;j<nums2.size();j++){
-        //         if(nums1[i]==nums2[j]){
-        //             while(j!=nums2.size()){
-        //                 if(nums1[i]<nums2[j]){
-        //                     val=nums2[j];
-        //                     break;
-        //                 }
-        //                 j++;
-        //             }
-        //             break;
-        //         }
-
-        //     }
-        //     v.push_back(val);
-        // }
-        // return v;
-        
-        vector<int>ans;
+        unordered_map<int,int>mpp;
         stack<int>st;
-        map<int,int>mpp;
-        st.push(-1);
-        for(int i=nums2.size()-1;i>-1;i--){
-            while(!st.empty() && st.top()<=nums2[i]){
+        for(int i=nums2.size()-1;i>=0;i--){
+            int nge=-1;
+            while(!st.empty()){
+                if(nums2[i]<st.top()){
+                    nge=st.top();
+                    break;
+                }
                 st.pop();
             }
-            if(!st.empty() && st.top()>nums2[i]){
-                mpp[nums2[i]]=st.top();
-            }
-            else mpp[nums2[i]]=-1;
             st.push(nums2[i]);
+            mpp[nums2[i]]=nge;
         }
-        for(auto it:nums1){
-            ans.push_back(mpp[it]);
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(mpp[nums1[i]]);
         }
         return ans;
     }
