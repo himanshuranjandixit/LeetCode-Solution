1ad6583d81878eacd97dc41fef13ec83334ed49a
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-05-21 10:47:31 +0530

    Time: 7 ms (7.78%), Space: 59.2 MB (91.22%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,46 +1,61 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        vector<int>v(k,0);
+        // vector<int>v(k,0);
+        // int l=0;
+        // int r=0;
+        // int indx=0;
+        // int small=0;
+        // int ans=0;
+        // int org=k;
+        // if(k==0){
+        //     while(r<nums.size()){
+        //         if(nums[r]==0){
+        //             while( r<nums.size() && nums[r]==0){
+        //                 r++;
+        //             }
+        //             l=r;
+        //             if(l!=nums.size()) ans=max(ans,1);
+        //         }
+        //         else{
+        //             r++;
+        //         }
+        //         ans=max(ans,r-l);
+        //     }
+        //     return ans;
+        // }
+        // while(r<nums.size()){
+        //     if(nums[r]==0 && k>0){
+        //         v[indx]=r;
+        //         indx=(indx+1)%org;
+        //         r++;
+        //         k--;
+        //     }
+        //     else if(nums[r]==0 && k==0){
+        //         l=v[small]+1;
+        //         small=(small+1)%org;
+        //         v[indx]=r;
+        //         indx=(indx+1)%org;
+        //         r++;
+        //     }
+        //     else{
+        //         r++;
+        //     }
+        //     ans=max(ans,r-l);
+        // }
+        // return ans;
+
         int l=0;
         int r=0;
-        int indx=0;
-        int small=0;
+        int zeroes=0;
         int ans=0;
-        int org=k;
-        if(k==0){
-            while(r<nums.size()){
-                if(nums[r]==0){
-                    while( r<nums.size() && nums[r]==0){
-                        r++;
-                    }
-                    l=r;
-                    if(l!=nums.size()) ans=max(ans,1);
-                }
-                else{
-                    r++;
-                }
-                ans=max(ans,r-l);
-            }
-            return ans;
-        }
         while(r<nums.size()){
-            if(nums[r]==0 && k>0){
-                v[indx]=r;
-                indx=(indx+1)%org;
-                r++;
-                k--;
+            if(nums[r]==0) zeroes++;
+            while(zeroes>k){
+                if(nums[l]==0) zeroes--;
+                l++;
             }
-            else if(nums[r]==0 && k==0){
-                l=v[small]+1;
-                small=(small+1)%org;
-                v[indx]=r;
-                indx=(indx+1)%org;
-                r++;
-            }
-            else{
-                r++;
-            }
+            r++;
             ans=max(ans,r-l);
         }
         return ans;
