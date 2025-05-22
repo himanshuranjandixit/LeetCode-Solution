4f47706fb4fc48db31b692ad9c2bb328fd1de577
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-05-22 10:51:22 +0530

    Time: 0 ms (100%), Space: 20.3 MB (98.16%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int indx,vector<int>& dp){
-        if(indx>=nums.size()-1) return dp[indx]=0;
-        if(dp[indx]!=-1) return dp[indx];
-        int ans=1e9;
-        for(int i=1;i<=nums[indx];i++){
-            if(indx+i<nums.size()){
-                ans= min(ans,1+solve(nums,indx+i,dp));
-            }
-        } 
-        return dp[indx]=ans;
-    }
+    // int solve(vector<int>& nums, int indx,vector<int>& dp){
+    //     if(indx>=nums.size()-1) return dp[indx]=0;
+    //     if(dp[indx]!=-1) return dp[indx];
+    //     int ans=1e9;
+    //     for(int i=1;i<=nums[indx];i++){
+    //         if(indx+i<nums.size()){
+    //             ans= min(ans,1+solve(nums,indx+i,dp));
+    //         }
+    //     } 
+    //     return dp[indx]=ans;
+    // }
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        int count=0;
-        solve(nums,0,dp);
-        return dp[0];
+        // vector<int>dp(nums.size(),-1);
+        // int count=0;
+        // solve(nums,0,dp);
+        // return dp[0];
+        int l=0;
+        int r=0;int ans=0;
+        while(r<nums.size()-1){
+            int maxi=0;
+            for(int i=l;i<=r;i++){
+                maxi=max(i+nums[i],maxi);
+            }
+            l=r+1;
+            r=maxi;
+            ans++;
+        }
+        return ans;
+
         
     }
 };
