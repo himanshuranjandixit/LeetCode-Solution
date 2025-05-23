fd7f0449451e15446e844be57b5d5f6fb7659b1c
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-05-23 14:54:30 +0530

    Time: 13 ms (65.18%), Space: 13 MB (85.44%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,63 +1,108 @@
 class Solution {
-public:
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> s(wordList.begin(), wordList.end());  
-        if (s.find(endWord) == s.end()) return {};  // If endWord is not in wordList, return empty
-
-        unordered_map<string, vector<string>> par;  // Stores parent words for each transformation
-        unordered_set<string> level;  // Tracks words added in the current BFS level
-        queue<string> q;
-        q.push(beginWord);
-        bool found = false;  // Flag to indicate if shortest paths are found
-
-        while (!q.empty() && !found) {
-            int sz = q.size();
-            unordered_set<string> newLevel;  // Track words visited in this level
-            while (sz--) {
-                string word = q.front();
-                q.pop();
-
-                string org = word;  // Store original word
-                for (int i = 0; i < word.size(); i++) {
-                    char oldChar = word[i];
-                    for (char ch = 'a'; ch <= 'z'; ch++) {
-                        if (ch == oldChar) continue;
-                        word[i] = ch;
-
-                        if (s.find(word) != s.end()) {
-                            if (par.find(word) == par.end()) {
-                                q.push(word);
-                                newLevel.insert(word);
-                            }
-                            par[word].push_back(org);  // Store parent for backtracking
-                            if (word == endWord) found = true;  // Stop BFS once we reach endWord
-                        }
-                    }
-                    word[i] = oldChar;  // Restore original character
+    unordered_map<string,int>mpp;
+    vector<vector<string>>ans;
+    string b;
+private:
+    void dfs(string word, vector<string>& seq){
+        if(word==b){
+            reverse(seq.begin(),seq.end());
+            ans.push_back(seq);
+            reverse(seq.begin(),seq.end());
+            return ;
+        }
+        int step = mpp[word];
+        for(int i=0;i<word.size();i++){
+            char org = word[i];
+            for(char ch = 'a'; ch<='z' ; ch++){
+                word[i]=ch;
+                if(mpp.find(word)!=mpp.end() && mpp[word]+1 == step){
+                    seq.push_back(word);
+                    dfs(word,seq);
+                    seq.pop_back();
                 }
             }
-            for (const string& w : newLevel) s.erase(w);  // Remove words visited in this level
+            word[i]=org;
         }
-
-        vector<vector<string>> ans;
-        if (found) {
-            vector<string> vec;
-            backtrack(ans, vec, endWord, beginWord, par);
-        }
-        return ans;
     }
 
-private:
-    void backtrack(vector<vector<string>>& ans, vector<string>& vec, string word, string& beginWord, 
-                   unordered_map<string, vector<string>>& par) {
-        vec.push_back(word);
-        if (word == beginWord) {
-            ans.push_back(vector<string>(vec.rbegin(), vec.rend()));  // Reverse path before adding
-        } else {
-            for (const string& parent : par[word]) {
-                backtrack(ans, vec, parent, beginWord, par);
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        // queue<vector<string>>q; //{{startword,step},string}
+        // unordered_set<string>st(wordList.begin(),wordList.end());
+        // st.erase(beginWord);
+        // vector<string>v;
+        // v.push_back(beginWord);
+        // q.push(v);
+        // int lvl=1;
+        // int minlvl=INT_MAX;
+        // unordered_set<string>temp;
+        // vector<vector<string>>ans;
+        // while(!q.empty()){
+        //     vector<string>vec=q.front();
+        //     string word =vec.back();
+        //     q.pop();
+        //     if(vec.size()>lvl){
+        //         for(auto const& wrd :temp){
+        //             st.erase(wrd);
+        //         }
+        //         temp.clear();
+        //         lvl=vec.size();
+        //         if(lvl>minlvl) break;
+        //     }
+        //     if(word==endWord && vec.size()<=minlvl){
+        //         ans.push_back(vec);
+        //         minlvl=vec.size();
+        //         continue;
+        //     }
+        //     for(int i=0;i<word.size();i++){
+        //         char org = word[i];
+        //         for(char ch ='a'; ch<='z';ch++){
+        //             word[i]=ch;
+        //             if(st.find(word)!=st.end()){
+        //                 vector<string>neww=vec;
+        //                 neww.push_back(word);
+        //                 q.push(neww);
+        //                 temp.insert(word);
+        //             }
+        //         }
+        //         word[i]=org;
+        //     }
+        // }
+        // return ans;
+
+        b =beginWord;
+        mpp[beginWord]=1;
+        queue<string>q;
+        unordered_set<string>st(wordList.begin(),wordList.end());
+        q.push(beginWord);
+        st.erase(beginWord);
+        while(!q.empty()){
+            string word = q.front();
+            q.pop();
+            int step =mpp[word];
+            if(word==endWord) break;
+            for(int i=0;i<word.size();i++){
+                char org = word[i];
+                for(char ch = 'a'; ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        mpp[word]=step+1;
+                        st.erase(word);
+                        q.push(word);
+                    }
+                }
+                word[i]=org;
             }
         }
-        vec.pop_back();
+        if(mpp.find(endWord)!=mpp.end()){
+            vector<string>seq;
+            seq.push_back(endWord);
+            dfs(endWord,seq);
+
+
+        }
+        return ans;
+
+
     }
 };
