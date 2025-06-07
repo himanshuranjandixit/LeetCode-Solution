c282fd7168f55c770e7384ba5773b965f0749791
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-06-07 16:09:43 +0530

    Time: 0 ms (100%), Space: 24.3 MB (20.99%) - LeetHub

--- 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
+++ 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
+    bool solve(vector<int>&nums, __int128 product,__int128 target,int indx){
+        if(product>target) return false;
+        if(product==target) return true;
+        if(indx==nums.size()) return false;
+        if(solve(nums,product*nums[indx],target,indx+1)) return true;
+        if(solve(nums,product,target,indx+1)) return true;
+        return false;
+
+
+    }
     bool checkEqualPartitions(vector<int>& nums, __int128 target) {
         __int128 product=1;
         for(int i=0;i<nums.size();i++){
             product*=(__int128)nums[i];
-            if(nums[i]>target) return false;
+            if(product>target*target) return false;
         }
-        if(target*target==product) return true;
-        return false;
+        if(product!=target*target) return false;
+        return solve(nums,1,target,0);
         
         
     }
