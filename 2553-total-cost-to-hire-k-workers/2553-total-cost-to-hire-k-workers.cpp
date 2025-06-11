0c7332d4839a35dbbb48d3c58341a97eb58018f1
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-06-11 18:15:03 +0530

    Time: 36 ms (76.87%), Space: 76.7 MB (91.42%) - LeetHub

---
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        int n=costs.size();
+        int l= 0;
+        int r=n-1;
+        priority_queue<int,vector<int>,greater<>>left,right;
+        while(left.size()<candidates && l<r){
+            left.push(costs[l]);
+            l++;
+        }
+        while(right.size()<candidates && r>=l){
+            right.push(costs[r]);
+            r--;
+        }
+
+        long long ans = 0;
+        while(k--){
+            if(right.empty() || (!left.empty() && left.top()<=right.top())){
+                ans += left.top(); left.pop();
+                if(l<=r) left.push(costs[l++]);
+            }
+            else{
+                ans+=right.top();
+                right.pop();
+                if(l<=r) right.push(costs[r--]);
+            }
+        }
+
+        return ans;
+    }
+};
