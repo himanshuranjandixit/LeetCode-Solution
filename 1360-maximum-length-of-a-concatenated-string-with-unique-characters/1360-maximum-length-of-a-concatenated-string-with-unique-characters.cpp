2c6249b3c6f1cb8540b5df360d33d9d8c179ade1
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-06-16 10:14:20 +0530

    Time: 45 ms (61.79%), Space: 46.7 MB (54.78%) - LeetHub

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int solve(vector<string>& arr, int indx,unordered_set<char>& st){
+    unordered_set<char>st;
+    int solve(vector<string>& arr, int indx,unordered_map<char,int>& mpp){
         if(indx==arr.size()) return 0;
         int not_taken=0;int taken=0;
-        not_taken = solve(arr,indx+1,st);
+        not_taken = solve(arr,indx+1,mpp);
         bool possible=true;
-        unordered_set<char>temp(st);
         for(auto it : arr[indx]){
-            if(temp.count(it)){
+            if(mpp[it]>0 || st.count(it)){
                 possible = false;
                 break;
             }
-            temp.insert(it);
+            st.insert(it);
         }
+        st.clear();
         if(possible){
-            taken = arr[indx].size() + solve(arr,indx+1,temp);
+            for(auto it : arr[indx]) mpp[it]++;
+            taken = arr[indx].size() + solve(arr,indx+1,mpp);
+            for(auto it : arr[indx]) mpp[it]--;
         }
         return max(taken,not_taken);
 
 
     }
     int maxLength(vector<string>& arr) {
-        unordered_set<char>st;
-        return solve(arr,0,st);
+        unordered_map<char,int>mpp;
+        return solve(arr,0,mpp);
         
     }
 };
