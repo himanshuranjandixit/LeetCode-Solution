0ca3bb769cf2eaf62a4063fdd37e99d79888bf09
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-06-26 13:03:58 +0530

    Time: 85 ms (52.03%), Space: 14.5 MB (54.98%) - LeetHub

--- 0869-similar-string-groups/0869-similar-string-groups.cpp
+++ 0869-similar-string-groups/0869-similar-string-groups.cpp
@@ -21,23 +21,54 @@ public:
         return diff==2 || diff==0;
 
     }
-    int numSimilarGroups(vector<string>& strs) {
-        int n = strs.size();
-        parent.resize(n);
-        for(auto i=0;i<n;i++){
-            parent[i]=i;
+
+    void dfs(vector<vector<int>>& adj, vector<int>& vis, int node){
+        vis[node]=1;
+        for(auto it : adj[node]){
+            if(!vis[it]) dfs(adj,vis,it);
         }
-        count=n;
+    }
+    int numSimilarGroups(vector<string>& strs) {
+        // int n = strs.size();
+        // parent.resize(n);
+        // for(auto i=0;i<n;i++){
+        //     parent[i]=i;
+        // }
+        // count=n;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(Similar(strs[i],strs[j])) {
+        //             unionByRank(i,j);
+
+        //         }
+        //     }
+
+        // }
+        // return count;
+        int n = strs.size();
+        vector<pair<int,int>>v;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(Similar(strs[i],strs[j])) {
-                    unionByRank(i,j);
-
+                if(Similar(strs[i],strs[j])){
+                    v.push_back({i,j});
                 }
             }
-
+        }
+        vector<vector<int>>adj(n);
+        for(auto it :v){
+            adj[it.first].push_back(it.second);
+            adj[it.second].push_back(it.first);
+        }
+        vector<int>vis(n,0);
+        count=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(adj,vis,i);
+                count++;
+            }
         }
         return count;
+
 
         
     }
