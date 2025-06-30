261dfa7b61f19163caafa10895472b364a28c23e
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-06-30 21:45:43 +0530

    Time: 36 ms (98.09%), Space: 52.4 MB (99.2%) - LeetHub

---
+++ 3826-maximum-profit-from-valid-topological-order-in-dag/3826-maximum-profit-from-valid-topological-order-in-dag.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int n;
+    vector<int> score;
+    vector<int> prereq; 
+    unordered_map<int, int> memo;
+
+    int dfs(int mask, int pos) {
+        if (mask == (1 << n) - 1) return 0;
+        if (memo.count(mask)) return memo[mask];
+        int maxProfit = LLONG_MIN;
+        for (int i = 0; i < n; i++) {
+            if ((mask & (1 << i)) == 0) {
+                if ((prereq[i] & mask) == prereq[i]) {
+                    int curr = score[i] * pos + dfs(mask | (1 << i), pos + 1);
+                    maxProfit = max(maxProfit, curr);
+                }
+            }
+        }
+        return memo[mask] = maxProfit;
+    }
+
+    int maxProfit(int _n, vector<vector<int>>& edges, vector<int>& _score) {
+        if(edges.empty()){
+            sort(_score.begin(),_score.end());
+            int ans=0;
+            for(int i=0;i<_n;i++){
+                ans += _score[i]*(i+1);
+            }
+            return ans ;
+        }
+
+
+        n= _n;
+        score=_score;
+        prereq.assign(n, 0);
+        memo.clear();
+        for (auto &e : edges) {
+            int u = e[0], v = e[1];
+            prereq[v] |= (1 << u);
+        }
+
+        return dfs(0, 1);
+    }
+
+};
