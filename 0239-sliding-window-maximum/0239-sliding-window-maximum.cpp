6a89928462ffcba96b5816c163a6401071bda928
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-07-05 16:57:33 +0530

    Time: 138 ms (12.84%), Space: 154.8 MB (11.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,53 +1,42 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // pair<int,int>maxi={INT_MIN,-1};
-        // for(int i=0;i<k;i++){
-        //     if(maxi.first<nums[i]){
-        //         maxi={nums[i],i};
-        //     }
-        // }
-        // vector<int>ans;
-        // pair<int,int>temp={INT_MIN,-1};
-        // int j=0;
-        // ans.push_back(maxi.first);
-        // for(int i=k;i<nums.size();i++){
-        //     j=i-k+1;
-        //     int maxx;
-        //     if(nums[i]>temp.first){
-        //         temp={nums[i],i};
-        //     }
-        //     if(maxi.second<j){
-        //         maxx=temp.first;
-        //         maxi=temp;
-        //         temp={INT_MIN,-1};
-        //     }
-        //     else if(nums[i]>maxi.first){
-        //         maxi={nums[i],i};
-        //         temp={INT_MIN,-1};
-        //     }
-        //     ans.push_back(maxi.first);
-        // }
-        // return ans;
-        
-        priority_queue<pair<int,int>>pq;
-        for(int i=0;i<k;i++){
-            pq.push({nums[i],i});
+    void buildSegmentTree(int i, int l, int r, vector<int>& segmentTree,vector<int>& arr) {
+        if(l == r) { // reached at leaf node
+            segmentTree[i] = arr[l];
+            return;
         }
-        int j=0;
+        
+        int mid = l + (r-l)/2;
+        buildSegmentTree(2*i+1, l, mid, segmentTree, arr); // creating left child
+        buildSegmentTree(2*i+2, mid+1, r, segmentTree, arr); // creating right child
+        segmentTree[i] = max(segmentTree[2*i + 1],segmentTree[2*i + 2]);
+    }
+    int querySegmentTree(int start, int end, int i, int l, int r, vector<int>& segmentTree) {
+        if(l > end || r < start){ // range is out of bound
+            return INT_MIN;
+        }
+        
+        if(l >= start && r <= end) {  // overlapping complete range
+            return segmentTree[i];
+        }
+        
+        int mid = l + (r-l)/2;
+        return max(querySegmentTree(start, end, 2*i+1, l,mid,segmentTree),querySegmentTree(start, end, 2*i+2, mid+1, r, segmentTree));
+    }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>segmentTree(4*n);
+        buildSegmentTree(0,0,n-1,segmentTree,nums);
         vector<int>ans;
-        ans.push_back(pq.top().first);
-        for(int i=k;i<nums.size();i++){
-            int j=i-k+1;
-            if(pq.top().second<j){
-                pq.pop();
-            }
-            pq.push({nums[i],i});
-            while(!pq.empty() && pq.top().second<j){
-                pq.pop();
-            }
-            ans.push_back(pq.top().first);
+        for(int i=0;i<n;i++){
+            int start = i;
+            int end = i+k-1;
+            if(i+k-1==n) break;
+            
+            int val = querySegmentTree(start,end,0,0,n-1,segmentTree);
+            ans.push_back(val); 
         }
         return ans;
+       
     }
 };
