5998f7dfa6cd941547e0f9887a384440c2ec25a9
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-07-07 09:10:47 +0530

    Time: 34 ms (99.52%), Space: 74.3 MB (100%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int maxEvents(vector<vector<int>>& events) {
-        vector<int>v1 = {1,1};
-        if(events.size()>1000 && events[0] == v1)return 100000;
-        sort(events.begin(),events.end(),[](const vector<int>& a, const vector<int>& b){
-            // if(a[0]!=b[0]) return a[0] < b[0];
-            return a[1] < b[1];
+    int parent[100005] = {0};
+    int find(int day) {
+        // if (day > 100000) return day; 
+        if(parent[day] == 0) return day; 
+        return parent[day] = find(parent[day]); 
+    }
 
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
         });
-        vector<int>calender(1e5+1,0);
-        int i=0;
-        int res=0;
-        while(i<events.size()){
-            int start_time = events[i][0];
-            int end_time = events[i][1];
-            for(int d=start_time ; d<= end_time ;d++){
-                if(!calender[d]){
-                    res++;
-                    calender[d]=1;
-                    break;
-                }
+
+        int res = 0;
+
+        for(auto& e : events){
+            int day = find(e[0]); 
+            if(day <= e[1]){    
+                parent[day] = day + 1;
+                res++;
             }
-            i++;
         }
+
         return res;
     }
 };
