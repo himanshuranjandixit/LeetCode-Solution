d1388821b61baaff147fc146fd37350642b1f608
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-07-16 21:27:26 +0530

    Time: 275 ms (45.21%), Space: 19.2 MB (62.06%) - LeetHub

---
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/3271-count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> countOfPairs(int n, int x, int y) {
+        vector<vector<int>>adj(n,vector<int>(n,INT_MAX));
+        for(int i=0;i<n-1;i++){
+            adj[i][i+1]=1;
+            adj[i+1][i]=1;
+        }
+        if(abs(x-y)!=1){
+            adj[x-1][y-1]=1;
+            adj[y-1][x-1]=1;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    if(adj[j][i]== INT_MAX || adj[i][k]==INT_MAX) continue;
+                    adj[j][k] = min(adj[j][k],adj[j][i] + adj[i][k]);
+                }
+            }
+        }
+        vector<int>ans(n,0);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<i+1 << "to "<<j+1 << " "<< adj[i][j];
+        //         cout<< endl;
+        //     }
+        // }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                int step = adj[i][j];
+                if(step-1<n) ans[step-1]++;
+
+            }
+        }
+        return ans;
+
+
+        
+    }
+};
