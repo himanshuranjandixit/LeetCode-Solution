52d9f1ee2a60b9581d9f9ae18cb0d9f173bbdf54
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-07-25 15:24:43 +0530

    Time: 16 ms (86.27%), Space: 19.9 MB (81.29%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        if (x < y) {
-            int temp = x;
-            x = y;
-            y = temp;
-            reverse(s.begin(), s.end());
+        if(x < y){
+            swap(x,y);
+            reverse(s.begin(),s.end());
         }
-
         int aCount = 0, bCount = 0, totalPoints = 0;
-
-        for (int i = 0; i < s.size(); ++i) {
-            char currentChar = s[i];
-
-            if (currentChar == 'a') {
-                ++aCount;
-            } 
-            else if (currentChar == 'b') {
-                if (aCount > 0) {
-                    --aCount;
-                    totalPoints += x;
-                }
-                else  ++bCount;
-                
-            }
+        for(int i = 0; i < s.size(); ++i){
+            char ch = s[i];
+            if (ch == 'a') aCount++;
             else{
-                totalPoints += min(bCount, aCount) * y;
-                aCount = bCount = 0;
+                if(ch == 'b'){
+                    if(aCount > 0){
+                        aCount--;
+                        totalPoints += x;
+                    }
+                    else  ++bCount;
+                }
+                else{
+                    totalPoints += min(bCount, aCount) * y;
+                    aCount = bCount = 0;
+                }
             }
         }
         totalPoints += min(bCount, aCount) * y;
