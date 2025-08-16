85c4dbeafb6d16e6dbcd17f1b5c356d564c395f9
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-08-16 11:53:46 +0530

    Time: 0 ms (100%), Space: 22.8 MB (61.69%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -10,26 +10,26 @@ public:
         if(ratings[0]>ratings[1])v[0]++;
         for(int i=1;i<ratings.size()-1;i++){
             if(ratings[i-1]<ratings[i]){
-                while(v[i]<=v[i-1]) v[i]++;
+                v[i] = max(v[i],v[i-1]+1);
             }
             if(ratings[i]>ratings[i+1]){
-                while(v[i]<=v[i+1]) v[i]++;
+                v[i]= max(v[i],v[i+1]+1);
             }
         }
         if(ratings[v.size()-1]>ratings[v.size()-2]){
-            while(v[v.size()-1]<=v[v.size()-2]) v[v.size()-1]++;
+            v[v.size()-1] = max(v[v.size()-1],v[v.size()-2]+1);
         }
 
         for(int i=ratings.size()-2;i>=1;i--){
             if(ratings[i+1]<ratings[i]){
-                while(v[i]<=v[i+1]) v[i]++;
+                v[i] = max(v[i],v[i+1]+1);
             }
             if(ratings[i]>ratings[i-1]){
-                while(v[i]<=v[i-1]) v[i]++;
+                v[i] = max(v[i],v[i-1]+1);
             }
         }
         if(ratings[0]>ratings[1]){
-            while(v[0]<=v[1]) v[0]++;
+            v[0] =max(v[0],v[1]+1);
         }
         int ans=accumulate(v.begin(),v.end(),0);
         return ans;
