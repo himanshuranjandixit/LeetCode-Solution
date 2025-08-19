dcd27331cc9c83e96326c4b08a166f78dc99a4b2
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-08-19 11:28:53 +0530

    Time: 689 ms (13.32%), Space: 187.8 MB (46.11%) - LeetHub

--- 2213-find-all-people-with-secret/2213-find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/2213-find-all-people-with-secret.cpp
@@ -6,8 +6,8 @@ public:
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
-        vector<int>ans(n,0);
-        ans[0]=1;
+        vector<int>ans;
+        vector<int>vis(n,0);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
         pq.push({0,firstPerson});
         pq.push({0,0});
@@ -15,18 +15,15 @@ public:
             int person = pq.top().second;
             int time = pq.top().first;
             pq.pop();
-            ans[person]=1;
+            if(!vis[person]) ans.push_back(person);
+            vis[person]=1;
             for(auto it:adj[person]){
-                if(ans[it.first]==0 && it.second>=time){
+                if(vis[it.first]==0 && it.second>=time){
                     pq.push({it.second,it.first});
                 }
             }
         }
-        vector<int>res;
-        for(int i=0;i<n;i++){
-            if(ans[i]) res.push_back(i);
-        }
-        return res;
+        return ans;
         
     }
 };
