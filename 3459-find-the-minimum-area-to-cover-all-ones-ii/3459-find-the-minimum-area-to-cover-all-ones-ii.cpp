36b3cb9b83592c45b7af4d0bd59c1d1c564832ee
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-08-24 17:33:01 +0530

    Time: 95 ms (35.16%), Space: 25.1 MB (34.07%) - LeetHub

--- 3459-find-the-minimum-area-to-cover-all-ones-ii/3459-find-the-minimum-area-to-cover-all-ones-ii.cpp
+++ 3459-find-the-minimum-area-to-cover-all-ones-ii/3459-find-the-minimum-area-to-cover-all-ones-ii.cpp
@@ -1,85 +1,70 @@
 class Solution {
 public:
-    vector<vector<int>> rotateClockWise(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<vector<int>> rotatedGrid(n, vector<int>(m));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                rotatedGrid[j][m - i - 1] = grid[i][j];
-            }
-        }
-
-        return rotatedGrid;
-    }
-
-    int minimumArea(int startRow, int endRow, int startCol, int endCol, vector<vector<int>>& grid) {
-        int m = grid.size();    // Rows
-        int n = grid[0].size(); // Columns
-
-        int minRow = m;
-        int maxRow = -1;
-        int minCol = n;
-        int maxCol = -1;
-
-        for (int i = startRow; i < endRow; i++) {
-            for (int j = startCol; j < endCol; j++) {
-                if (grid[i][j] == 1) {
-                    minRow = min(minRow, i);
-                    maxRow = max(maxRow, i);
-                    minCol = min(minCol, j);
-                    maxCol = max(maxCol, j);
+    int minArea(vector<vector<int>>& grid, int startrow, int endrow, int startcol, int endcol){
+        int area;
+        int n = grid.size();
+        int m = grid[0].size();
+        int minx=n;
+        int miny =m;
+        int maxx=0;
+        int maxy =0;
+        for(int i=startrow;i<=endrow;i++){
+            for(int j=startcol;j<=endcol;j++){
+                if(grid[i][j]==1){
+                    minx =min(minx,i);
+                    miny = min(miny,j);
+                    maxx=max(maxx,i);
+                    maxy=max(maxy,j);
                 }
             }
         }
-        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+        if (minx > maxx || miny > maxy) return 1e7;
+        return (maxx-minx+1)*(maxy-miny+1);
     }
-
-    // O(m^2 * n^2)
-    int helper(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        int result = INT_MAX;
-
-        for (int rowSplit = 1; rowSplit < m; rowSplit++) {
-            for (int colSplit = 1; colSplit < n; colSplit++) {
-
-                int top = minimumArea(0, rowSplit, 0, n, grid);
-                int bottomLeft = minimumArea(rowSplit, m, 0, colSplit, grid);
-                int bottomRight = minimumArea(rowSplit, m, colSplit, n, grid);
-
-                result = min(result, top + bottomLeft + bottomRight);
-
-                int topLeft = minimumArea(0, rowSplit, 0, colSplit, grid);
-                int topRight = minimumArea(0, rowSplit, colSplit, n, grid);
-                int bottom = minimumArea(rowSplit, m, 0, n, grid);
-
-                result = min(result, topLeft + topRight + bottom);
-            }
-        }
-
-        for (int split1 = 1; split1 < m; split1++) {
-            for (int split2 = split1 + 1; split2 < m; split2++) {
-                int top = minimumArea(0, split1, 0, n, grid);
-                int middle = minimumArea(split1, split2, 0, n, grid);
-                int bottom = minimumArea(split2, m, 0, n, grid);
-
-                result = min(result, top + middle + bottom);
-            }
-        }
-        return result;
-    }
-
     int minimumSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans=m*n;
+        //case 1
 
-        int result = helper(grid);
+        for(int rowsplit = 0 ; rowsplit < n-1 ; rowsplit++){
+            for(int colsplit = 0 ; colsplit < m-1 ; colsplit++){
+                int top = minArea(grid,0,rowsplit,0,m-1);
+                int bottomleft = minArea(grid,rowsplit+1,n-1,0,colsplit);
+                int bottomright = minArea(grid,rowsplit+1,n-1,colsplit+1,m-1);
+                ans=min(ans,top+bottomleft+bottomright);
+                int topleft = minArea(grid,0,rowsplit,0,colsplit);
+                int topright = minArea(grid,0,rowsplit,colsplit+1,m-1);
+                int bottom = minArea(grid,rowsplit+1,n-1,0,m-1);
+                ans=min(ans,topleft+topright+bottom);
+                int lefttop = minArea(grid,0,rowsplit,0,colsplit);
+                int leftbottom = minArea(grid,rowsplit+1,n-1,0,colsplit);
+                int right = minArea(grid,0,n-1,colsplit+1,m-1);
+                ans=min(ans,lefttop+leftbottom+right);
+                int left = minArea(grid,0,n-1,0,colsplit);
+                int righttop = minArea(grid,0,rowsplit,colsplit+1,m-1);
+                int rightbottom = minArea(grid,rowsplit+1,n-1,colsplit+1,m-1);
+                ans=min(ans,left+righttop+rightbottom);
+            }
+        }
+        for(int colsplit1 =0 ; colsplit1 < m-2 ; colsplit1++){
+            for(int colsplit2 =colsplit1+1 ; colsplit2<m-1;colsplit2++){
+                int left = minArea(grid,0,n-1,0,colsplit1);
+                int mid = minArea(grid,0,n-1,colsplit1+1,colsplit2);
+                int right = minArea(grid,0,n-1,colsplit2+1,m-1);
+                ans=min(ans,left+mid+right);
+            }
+        }
+        for(int rowsplit1 = 0; rowsplit1 < n-2; rowsplit1++){
+            for(int rowsplit2 = rowsplit1+1; rowsplit2 < n-1; rowsplit2++){
+                int top    = minArea(grid, 0, rowsplit1, 0, m-1);
+                int middle = minArea(grid, rowsplit1+1, rowsplit2, 0, m-1);
+                int bottom = minArea(grid, rowsplit2+1, n-1, 0, m-1);
+                ans = min(ans, top + middle + bottom);
+            }
+        }
+        return ans;
 
-        vector<vector<int>> rotatedGrid = rotateClockWise(grid);
-
-        result = min(result, helper(rotatedGrid));
-
-        return result;
+        
     }
 };
