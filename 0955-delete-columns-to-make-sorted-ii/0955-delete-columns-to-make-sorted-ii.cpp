d638c5ffe9773c8c7c7208e9eeb1cb32d1831a54
Himanshu Dixit <himanshuranjandixit@gmail.com>
2025-12-21 20:24:37 +0530

    Time: 0 ms (100%), Space: 12.9 MB (38.2%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -3,23 +3,23 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int n = strs.size();
         int m = strs[0].size();
-        vector<bool> fixed(n - 1, false);
+        vector<bool>fixed(n-1,false);
         int del = 0;
 
-        for (int c = 0; c < m; c++) {
+        for(int c = 0;c<m;c++){
             bool bad = false;
-            for (int i = 0; i < n - 1; i++) {
-                if (!fixed[i] && strs[i][c] > strs[i + 1][c]) {
+            for(int i = 0;i<n-1;i++){
+                if(!fixed[i] && strs[i][c]>strs[i + 1][c]){
                     bad = true;
                     break;
                 }
             }
-            if (bad) {
+            if(bad){
                 del++;
                 continue;
             }
-            for (int i = 0; i < n - 1; i++) {
-                if (!fixed[i] && strs[i][c] < strs[i + 1][c]) {
+            for(int i = 0;i<n-1;i++){
+                if(!fixed[i] && strs[i][c]<strs[i + 1][c]){
                     fixed[i] = true;
                 }
             }
