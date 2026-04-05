7b7b09f2f41ed744da914b8d493c1ec6d931d0c9
Himanshu Dixit <himanshuranjandixit@gmail.com>
2026-04-05 21:23:40 +0530

    Time: 2 ms (53.4%), Space: 10.2 MB (76.78%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y=0;
+        for(auto it:moves){
+            if(it=='U') y+=1;
+            if(it=='D') y-=1;
+            if(it=='L') x-=1;
+            if(it=='R') x+=1;
+        }
+        if(x==0 && y==0) return true;
+        return false;
+        
+    }
+};
